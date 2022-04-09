21d4e559923489f0553cbd7d8f1cdc58be2d2435
Kim Tae-song <kts@vps.server.local>
2022-04-09 15:28:23 +0000

    lint fix 0

--- contracts/staking/src/testing.rs
+++ contracts/staking/src/testing.rs
@@ -463,10 +463,7 @@ fn test_compute_reward() {
     let res = query(
         deps.as_ref(),
         mock_env(),
-        QueryMsg::StakerInfo { 
-            staker: "addr0000".to_string(),
-            block_time: None,
-        },
+        QueryMsg::StakerInfo { staker: "addr0000".to_string(), block_time: None },
     )
     .unwrap();
     let state: StakerInfoResponse = from_binary(&res).unwrap();
